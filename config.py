84739cfdf3c0de4af30435e8f64a50aaae03f680
Kratik Rathi <kratikrathi@gmail.com>
2025-04-06 15:15:33 -0400

    Project files upload

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
-Huggingface_token = os.getenv("HF_TOKEN", "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")
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the keys from environment
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+Huggingface_token = os.getenv("HF_TOKEN")
